b8c657d7fef4151ec32e31b9fd9ba4bf2080efd7
紗稀 <106290195+nwtrsk@users.noreply.github.com>
2022-10-28 08:24:03 +0000

    新しいテーブルの作成

--- select.js
+++ select.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test3.db');
 
 let sql = `
-select id, name, member_id from single;
+select single.id, single.name, member.name as name2 from single inner join member on single.member_id=member.id;
 `
 
 db.serialize( () => {
@@ -12,7 +12,7 @@ db.serialize( () => {
 			return;
 		}
 		for( let data of row ) {
-			console.log( data.id + ' : ' + data.name + ' : ' + data.member_id );
+			console.log( data.id + ' : ' + data.name + ' : ' + data.name2 );
 		}
 	});
 });
