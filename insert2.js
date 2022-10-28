0cc67ddabe57af2e27bccd882424a5b5720a6650
紗稀 <106290195+nwtrsk@users.noreply.github.com>
2022-10-28 16:00:28 +0000

    データベースの新規作成

--- insert2.js
+++ insert2.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test3.db');
 
 let sql = `
-insert into member ("名前", "期生", "生年月日", "出身", "選抜数", "参加シングル数") values ("伊藤万理華","1", "1996年2月20日", "神奈川", "11", "19");
+insert into member ("名前", "期生", "生年月日", "出身", "選抜数", "参加シングル数") values ("高山一実","1", "1994年2月8日", "千葉", "28", "28");
 `
 
 db.serialize( () => {
