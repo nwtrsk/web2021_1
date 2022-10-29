4fe5835b0d1fa6c457934400b8ede0a6c60a8bf7
紗稀 <106290195+nwtrsk@users.noreply.github.com>
2022-10-29 11:53:42 +0000

    テーブルの作成

--- insert3.js
+++ insert3.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test3.db');
 
 let sql = `
-insert into single ("シングル名", "発売日", "初日売上", "初週売上", "センター") values ("君の名は希望", "2013年3月13日", "20.4万枚", "24.2万枚", 3);
+insert into single ("シングル名", "発売日", "初日売上", "初週売上", "センター") values ("ハルジオンが咲く頃", "2016年3月23日", "59.0万枚", "75.0万枚", 20);
 `
 
 db.serialize( () => {
