bc26c2be74db46756b4a213ff1ecbd4355942626
紗稀 <106290195+nwtrsk@users.noreply.github.com>
2022-10-29 12:12:50 +0000

    テーブルの作成

--- insert3.js
+++ insert3.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test3.db');
 
 let sql = `
-insert into single ("シングル名", "発売日", "初日売上", "初週売上", "センター") values ("ハルジオンが咲く頃", "2016年3月23日", "59.0万枚", "75.0万枚", 20);
+insert into single ("シングル名", "発売日", "初日売上", "初週売上", "センター") values ("インフルエンサー", "2017年3月22日", "74.3万枚", "87.4万枚", 16);
 `
 
 db.serialize( () => {
---
+++ select4.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test3.db');
+
+let sql = `
+select single.id, "シングル名", "発売日", "初日売上", "初週売上", "名前" from single inner join member on "センター"=member.id order by "初週売上" desc;
+`
+
+db.serialize( () => {
+	db.all( sql, (error, row) => {
+		if(error) {
+			console.log('Error: ', error );
+			return;
+		}
+		for( let data of row ) {
+			console.log( data.id + ' : ' + data.シングル名 + ' : ' + data.発売日 + ' : ' + data.初日売上 + ' : ' + data.初週売上 + ' : ' + data.名前 );
+    }
+  });
+});
