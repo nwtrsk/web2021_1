0165fe99b3b74e2d2702d10ee5da4a45b16ce392
紗稀 <106290195+nwtrsk@users.noreply.github.com>
2022-11-11 08:43:51 +0000

    app7のデータのapp6への移行とapp7の削除

--- app6.js
+++ app6.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('test.db');
+const db = new sqlite3.Database('test3.db');
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
@@ -11,38 +11,116 @@ app.use(express.json())
 app.use(express.urlencoded({extend: true}));
 
 app.get("/", (req, res) => {
-  const message = "Hello world";
-  res.render('show', {mes:message});
+  const message = "こんにちは乃木坂46です";
+  res.render('show2', {mes:message});
 });
 
-app.get("/dbb", (req, res) => {
+app.get("/single", (req, res) => {
     db.serialize( () => {
-        db.all("select id, 都道府県, 人口 from example;", (error, row) => {
+        db.all("select single.id, シングル名, 発売日, 初日売上, 初週売上, センター from single ;", (error, row) => {
             if( error ) {
-                res.render('show', {mes:"エラーです"});
+                res.render('show2', {mes:"エラーです"});
             }
-            res.render('select', {data:row});
+            res.render('single', {data:row});
         })
     })
 })
+
+app.get("/member", (req, res) => {
+    db.serialize( () => {
+        db.all("select id,名前,期生,生年月日,出身,選抜数,参加シングル数 from member;", (error, row) => {
+            if( error ) {
+                res.render('show2', {mes:"エラーです"});
+            }
+            res.render('member', {data:row});
+        })
+    })
+})
+
 app.get("/top", (req, res) => {
     //console.log(req.query.pop);    // ①
     let desc = "";
-    if( req.query.desc ) desc = " desc";
-    let sql = "select id, 都道府県,人口 from example order by 人口" + desc + " limit " + req.query.pop + ";";
+    if( req.query.desc ) desc = "desc";
+    let sql = "select id, シングル名, 発売日, 初日売上, 初週売上, センター from single order by 初週売上" + desc + " limit " + req.query.pop + ";";
     //console.log(sql);    // ②
     db.serialize( () => {
         db.all(sql, (error, data) => {
             if( error ) {
-                res.render('show', {mes:"エラーです"});
+                res.render('show2', {mes:"エラーです"});
             }
             //console.log(data);    // ③
-            res.render('select', {data:data});
+            res.render('db', {data:data});
         })
     })
 })
+
+app.get("/sear", (req,res) => {
+  console.log(req.params);
+  db.serialize( () => {
+    db.all("select id,名前,期生,生年月日,出身,選抜数,参加シングル数 from member where 期生 =" + req.query.num + ";",(error,row) => {
+      if(error){
+        res.render('show2',{mes:"エラーです"});
+      }
+      res.render('db2',{data:row});
+    })
+  })
+})
+
+app.get("/db/:id", (req,res) => {
+  console.log(req.params);
+  db.serialize( () => {
+    db.all("select id,シングル名,発売日,初日売上,初週売上,センター from single where id =" + req.params.id + ";",(error,row) => {
+      if(error){
+        res.render('show2',{mes:"エラーです"});
+      }
+      res.render('db',{data:row});
+    })
+  })
+})
+
+app.get("/db2/:id", (req,res) => {
+  console.log(req.params);
+  db.serialize( () => {
+    db.all("select id,名前,期生,生年月日,出身,選抜数,参加シングル数 from member where id =" + req.params.id + ";",(error,row) => {
+      if(error){
+        res.render('show2',{mes:"エラーです"});
+      }
+      res.render('db2',{data:row});
+    })
+  })
+})
+
+app.post("/singleadd",(req,res) => {
+  let sql = 'insert into single(シングル名,発売日,初日売上,初週売上,センター)values("'+ req.body.name +'","'+ req.body.date +'","'+ req.body.datear +'","'+ req.body.weekear +'",'+ req.body.center +');'
+  console.log(sql);
+  db.serialize(() => {
+    db.run(sql,(error,row) =>{
+      console.log(error);
+      if(error){
+        res.render('show2',{mes:"エラーです"});
+      }
+      res.render('show2',{mes:"成功です"});
+    });
+  });
+  console.log(req.body);
+});
+
+app.post("/memberadd",(req,res) => {
+  let sql = 'insert into member(名前,期生,生年月日,出身,選抜数,参加シングル数)values("'+ req.body.name +'",' + req.body.period +',"'+ req.body.birth +'","'+ req.body.from +'",'+ req.body.selnum +','+ req.body.parnum +');'
+  console.log(sql);
+  db.serialize(() => {
+    db.run(sql,(error,row) =>{
+      console.log(error);
+      if(error){
+        res.render('show2',{mes:"エラーです"});
+      }
+      res.render('show2',{mes:"成功です"});
+    });
+  });
+  console.log(req.body);
+});
+
+app.listen(80, () => console.log("Example app listening on port 80!"));
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
-
-app.listen(8080, () => console.log("Example app listening on port 8080!"));
