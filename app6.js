65aaa276a190ad6f0915a73e7c39c2ed9f630417
紗稀 <106290195+nwtrsk@users.noreply.github.com>
2022-11-15 02:32:43 +0000

    選抜割合表示のページ追加

--- app6.js
+++ app6.js
@@ -90,6 +90,23 @@ app.get("/db2/:id", (req,res) => {
   })
 })
 
+app.get("/mem", (req, res) => {
+    //console.log(req.query.pop);    // ①
+    let desc = "";
+    if( req.query.desc ) desc = " desc";
+    let sql = "select id, 名前, 期生, 生年月日, 出身, cast(選抜数 as real) * 100 / cast(参加シングル数 as real) as result from member order by result" + desc + " limit " + req.query.pop + ";";
+    //console.log(sql);    // ②
+    db.serialize( () => {
+        db.all(sql, (error, data) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            //console.log(data);    // ③
+            res.render('mem', {data:data});
+        })
+    })
+})
+
 app.post("/singleadd",(req,res) => {
   let sql = 'insert into single(シングル名,発売日,初日売上,初週売上,センター)values("'+ req.body.name +'","'+ req.body.date +'","'+ req.body.datear +'","'+ req.body.weekear +'",'+ req.body.center +');'
   console.log(sql);
