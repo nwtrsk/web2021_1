ce8fb16443cf83b6d140aa25539a78ca2ff94022
紗稀 <106290195+nwtrsk@users.noreply.github.com>
2022-11-26 13:05:45 +0000

    ページデザイン

--- app6.js
+++ app6.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extend: true}));
 
 app.get("/", (req, res) => {
-  const message = "こんにちは乃木坂46です";
+  const message = "こんにちは坂道グループです";
   res.render('show', {mes:message});
 });
 
